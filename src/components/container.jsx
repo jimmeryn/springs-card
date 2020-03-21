a058aa403d7527fbcfed46bbf2c52127f6d39268
jimmeryn <jimmeryn@gmail.com>
2020-03-21 21:39:42 +0100

    top button added

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Ofert from "./ofert";
 import Partners from "./partners";
 import Contact from "./contact";
+import TopButton from "./topButton";
 
 const Container = () => (
   <div className="Container">
     <Ofert />
     <Partners />
     <Contact />
+    <TopButton />
   </div>
 );
 
