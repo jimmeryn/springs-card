13a463ee95b9e4c5cf280b47b270f1a51122dcd6
jimmeryn <jimmeryn@gmail.com>
2020-04-02 22:34:29 +0200

    Scrolling

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+};
+
 const Logo = () => (
-  <div className="logo">
+  <div className="logo" onClick={scrollToTop}>
     <div>
       <h1 className="logo-main">SEMA</h1>
       <span className="logo-underline"></span>
     </div>
-    <h4 className="logo-sub">SPRINGS</h4>
+    <h4 className="logo-sub">
+      <i>SPRINGS</i>
+    </h4>
   </div>
 );
 
